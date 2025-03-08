105ad23d0af3bd9dd28d0f0e6882c6acaf3ddd3f
goldenstain <xym@MSI>
2025-03-09 03:31:47 +0800

    replace the random method with a mordern cpp way

--- exercises/hw1/hello.cu
+++ exercises/hw1/hello.cu
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-__global__ void hello(){
-
-  printf("Hello from block: %u, thread: %u\n", FIXME);
+__global__ void hello() {
+    printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
 }
 
-int main(){
-
-  hello<<<FIXME>>>();
-  cudaDeviceSynchronize();
+int main() {
+    hello<<<20, 10>>>();
+    cudaDeviceSynchronize();
+    printf("end of execution.\n");
 }
-
