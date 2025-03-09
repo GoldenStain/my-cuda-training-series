6efdb7c1c32f961d47627b8f1ebb6ed908f6138d
goldenstain <xym@MSI>
2025-03-09 21:28:52 +0800

    finish hw2-stencil_1d

--- exercises/hw2/stencil_1d.cu
+++ exercises/hw2/stencil_1d.cu
@@ -8,9 +8,10 @@ using namespace std;
 #define BLOCK_SIZE 16
 
 __global__ void stencil_1d(int *in, int *out) {
-    __shared__ int temp[FIXME];
+    __shared__ int temp[BLOCK_SIZE+2*RADIUS];
+    // 因为我们把地址传进来的时候已经加了RADIUS，这里gindex就不用加了。
     int gindex = threadIdx.x + blockIdx.x * blockDim.x;
-    int lindex = FIXME;
+    int lindex = threadIdx.x + RADIUS;
 
     // Read input elements into shared memory
     temp[lindex] = in[gindex];
@@ -25,9 +26,10 @@ __global__ void stencil_1d(int *in, int *out) {
     // Apply the stencil
     int result = 0;
     for (int offset = -RADIUS; offset <= RADIUS; offset++)
-      result += temp[FIXME];
+      result += temp[lindex + offset];
 
     // Store the result
+    // 因为我们的d_in , d_out只偏移了RADIUS，所以最后RADIUS个取不到，就不会做多余操作。
     out[gindex] = result;
 }
 
@@ -40,7 +42,7 @@ int main(void) {
   int *d_in, *d_out; // device copies of a, b, c
 
   // Alloc space for host copies and setup values
-  int size = (FIXME) * sizeof(int);
+  int size = (N + 2*RADIUS) * sizeof(int);
   in = (int *)malloc(size); fill_ints(in, N + 2*RADIUS);
   out = (int *)malloc(size); fill_ints(out, N + 2*RADIUS);
 
@@ -53,7 +55,8 @@ int main(void) {
   cudaMemcpy(d_out, out, size, cudaMemcpyHostToDevice);
 
   // Launch stencil_1d() kernel on GPU
-  stencil_1d<<<N/BLOCK_SIZE,BLOCK_SIZE>>>(FIXME, FIXME);
+  // 注意这里也要加上偏移量RADIUS  不然两头就会偏大
+  stencil_1d<<<N/BLOCK_SIZE,BLOCK_SIZE>>>(d_in+RADIUS, d_out+RADIUS);
 
   // Copy result back to host
   cudaMemcpy(out, d_out, size, cudaMemcpyDeviceToHost);
