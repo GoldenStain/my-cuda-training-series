488d91a8aba9f20968c296aa05a2285ff6d7a4b4
goldenstain <xym@MSI>
2025-03-11 23:38:29 +0800

    finish hw2

--- exercises/hw3/vector_add.cu
+++ exercises/hw3/vector_add.cu
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <chrono>
+#include <iostream>
+#include <iomanip>
 
 // error checking macro
 #define cudaCheckErrors(msg)                                        \
@@ -15,13 +18,15 @@
 const int DSIZE = 32 * 1048576;
 // vector add kernel: C = A + B
 __global__ void vadd(const float *A, const float *B, float *C, int ds) {
+    // 注意这里的步长是gridDim.x * blockDim.x ，这是我们第一次遇到gridDim.x
     for (int idx = threadIdx.x + blockDim.x * blockIdx.x; idx < ds;
          idx += gridDim.x * blockDim.x) {  // a grid-stride loop
-         
-        }
+        C[idx] = A[idx] + B[idx];
+    }
 }
 
 int main() {
+    using namespace std::chrono;
     float *h_A, *h_B, *h_C, *d_A, *d_B, *d_C;
     h_A = new float[DSIZE];  // allocate space for vectors in host memory
     h_B = new float[DSIZE];
@@ -45,13 +50,16 @@ int main() {
     cudaCheckErrors("cudaMemcpy H2D failure");
     // cuda processing sequence step 1 is complete
     int threads = 512;  // modify this line for experimentation
-    int blocks = (DSIZE + threads - 1) /
-                 threads;  // modify this line for experimentation
+    int blocks = 16;     // modify this line for experimentation
+    auto start = high_resolution_clock::now();
     vadd<<<blocks, threads>>>(d_A, d_B, d_C, DSIZE);
+    auto end = high_resolution_clock::now();
+    duration<double, std::milli> elapsed = end - start;
+    std::cout << "It took " << std::fixed << std::setprecision(3) << elapsed.count() << "milliseconds" << std::endl;
     cudaCheckErrors("kernel launch failure");
     // cuda processing sequence step 2 is complete
     //  copy vector C from device to host:
-    cudaMemcpy(h_C, d_C, DSIZE * sizeof(float), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(h_C, d_C, DSIZE * sizeof(float), cudaMemcpyDeviceToHost);
     // cuda processing sequence step 3 is complete
     cudaCheckErrors("kernel execution failure or cudaMemcpy H2D failure");
     printf("A[0] = %f\n", h_A[0]);
