8e5d2501af4d35711acb194534fdf160dfbb167f
goldenstain <xym@MSI>
2025-03-15 17:47:23 +0800

    finish row_sums in hw5

--- exercises/hw5/matrix_sums.cu
+++ exercises/hw5/matrix_sums.cu
@@ -18,14 +18,25 @@ const size_t DSIZE = 16384;      // matrix side dimension
 const int block_size = 256;  // CUDA maximum is 1024
 // matrix row-sum kernel
 __global__ void row_sums(const float *A, float *sums, size_t ds){
-
-  int idx = threadIdx.x+blockDim.x*blockIdx.x; // create typical 1D thread index from built-in variables
-  if (idx < ds){
-    float sum = 0.0f;
-    for (size_t i = 0; i < ds; i++)
-      sum += A[idx*ds+i];         // write a for loop that will cause the thread to iterate across a row, keeeping a running sum, and write the result to sums
-    sums[idx] = sum;
-}}
+    int bidx = blockIdx.x, base = bidx * ds; // 表示行号
+    if (bidx < ds) {
+        __shared__ float sdata[block_size];
+        // 通过stride把整行都放进来
+        int tidx = threadIdx.x, now_idx = tidx;
+        sdata[tidx] = 0.0f;
+        while(now_idx < ds) {
+            sdata[tidx] += A[base+now_idx];
+            now_idx += blockDim.x;
+        }
+        for (unsigned int offset = blockDim.x/2; offset; offset>>=1) {
+            __syncthreads();
+            if (tidx < offset) // 只有前一半需要累加
+                sdata[tidx] += sdata[tidx + offset];
+        }
+        if (tidx == 0)
+            sums[bidx] = sdata[0];
+    }
+}
 // matrix column-sum kernel
 __global__ void column_sums(const float *A, float *sums, size_t ds){
 
@@ -55,7 +66,7 @@ int main(){
   cudaMemcpy(d_A, h_A, DSIZE*DSIZE*sizeof(float), cudaMemcpyHostToDevice);
   cudaCheckErrors("cudaMemcpy H2D failure");
   //cuda processing sequence step 1 is complete
-  row_sums<<<(DSIZE+block_size-1)/block_size, block_size>>>(d_A, d_sums, DSIZE);
+  row_sums<<<DSIZE, block_size>>>(d_A, d_sums, DSIZE);
   cudaCheckErrors("kernel launch failure");
   //cuda processing sequence step 2 is complete
   // copy vector sums from device to host:
