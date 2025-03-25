af1b678eaa750d7238bc5bdcf3d5208ec94ccb9c
goldenstain <xym@MSI>
2025-03-26 01:40:50 +0800

    add a wrong prefix_sum warp demo

---
+++ my_demo/warp_sum.cu
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// 复用之前的warp_reduce_sum
+template <int WARP_SIZE>
+__device__ __forceinline__ float warp_reduce_sum(float val) {
+    #pragma unroll
+    for (int offset = 1; offset < WARP_SIZE; offset <<= 1) {
+        float tmp = __shfl_up_sync(0xffffffff, val, offset);
+        printf("T%d: offset=%d, tmp=%.1f, val=%.1f->%.1f\n", 
+            threadIdx.x, offset, tmp, val, val + tmp);
+        val += tmp;
+    }
+    return val;
+}
+
+__global__ void warp_reduce_demo() {
+    const int idx = threadIdx.x;
+    float val = idx + 1; // 初始值1~32
+    
+    printf("-- Before reduce: T%d val=%.1f\n", idx, val);
+    float sum = warp_reduce_sum<32>(val);
+}
+
+int main() {
+    warp_reduce_demo<<<1, 32>>>(); // 单个Warp
+    cudaDeviceSynchronize();
+    return 0;
+}
